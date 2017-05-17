fdc582e7371d79f5c2c1b5c8df74a1a6478134e0
Thulio Philipe <thulioph@gmail.com>
2017-05-17 16:18:05 -0300

    First commit.

---
+++ src/js/main.js
@@ -0,0 +1,40 @@
+(function() {
+	'use strict';
+
+	const button = document.querySelector('button[type="submit"]');
+	const form = document.getElementById('knForm');
+	const input = document.querySelector('.input');
+
+	function sendData(obj) {
+		console.warn('The data is:', obj);
+	}
+
+	function getTags(tabUrl) {
+		let tags;
+
+		if (input.value) {
+			tags = input.value.split(',');
+		}
+
+		sendData({
+			tabUrl,
+			tags
+		});
+	}
+
+	function handleSubmitForm(evt) {
+		evt.preventDefault();
+
+		button.classList.add('is-loading');
+
+		if (chrome.tabs) {
+			chrome.tabs.query({ active: true }, (tab) => {
+				getTags(tab[0].url);
+			});
+		}
+	}
+
+	document.addEventListener('DOMContentLoaded', () => {
+		form.addEventListener('submit', handleSubmitForm, false);
+	}, false);
+})();
