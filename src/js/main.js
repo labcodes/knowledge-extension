2b1109c08bc25234588044aefa7112d2d6d4da9a
Thulio Philipe <thulioph@gmail.com>
2017-06-08 17:05:54 -0300

    Adds two buttons on notifications to share with Facebook or Twitter.

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,53 @@
 (function() {
 	'use strict';
 
-	const button = document.querySelector('button[type="submit"]');
+	const button = document.querySelector('#postLink');
 	const form = document.getElementById('formTags');
   const input = document.querySelector('#linkTags');
 
   // ====
 
+  function _shareWithFacebook(link) {
+    const url = `https://www.facebook.com/sharer/sharer.php?u=${link}`;
+    window.open(url, '_blank');
+  }
+
+  function _shareWithTwitter(link) {
+    const url = `https://twitter.com/intent/tweet?text=${link}`;
+    window.open(url, '_blank');
+  }
+
   function notification(obj) {
     let notificationId = new Date().getTime().toString();
+    const linkUrl = obj.url;
 
     let options = {
       type: 'basic',
-      title: obj.title,
-      message: obj.message,
-      iconUrl: './src/images/icon128.png'
+      title: 'Link postado com sucessso!',
+      message: obj.title,
+      iconUrl: './src/images/icon128.png',
+      buttons: [
+        {
+          title: 'I want to share with Facebook',
+          // iconUrl: './src/images/fbIcon.png'
+        },
+        {
+          title: 'I want to share with Twitter',
+          // iconUrl: './src/images/twIcon.png'
+        },
+      ],
     };
 
-    chrome.notifications.create(notificationId, options, (id) => {
-      console.warn('Notification ID:', id);
+    chrome.notifications.create(notificationId, options);
+
+    chrome.notifications.onButtonClicked.addListener((notifId, btnIndex) => {
+      if (notifId === notificationId) {
+        if (btnIndex === 0) {
+          _shareWithFacebook(linkUrl);
+        } else {
+          _shareWithTwitter(linkUrl);
+        }
+      }
     });
   }
 
@@ -26,25 +55,21 @@
     input.value = '';
     button.classList.remove('is-loading');
 
-    notification({
-      title: 'Success!',
-      message: 'Seu link foi postado com sucesso.'
-    });
+    notification(obj);
   }
 
 	function sendData(dataObj) {
     const myToken = localStorage.getItem('kn_ext');
-    dataObj.title = 'CHROME EXTENSIONS <3';
 
     let xhr = new XMLHttpRequest();
 
-    xhr.open('POST', 'https://link-notifications.herokuapp.com/api/links/create/', true);
+    xhr.open('POST', 'http://knowledge.labcodes.com.br/api/links/create/', true);
 
     xhr.setRequestHeader('accept', 'application/json');
     xhr.setRequestHeader('content-type', 'application/json');
     xhr.setRequestHeader('Authorization', `Token ${myToken}`);
 
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = () => {
       switch(xhr.readyState) {
         case 4:
           _handleResponse(JSON.parse(xhr.responseText));
